3ab4a5e73e19e6c779a8721da13289a3d90b78cc
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2025-05-01 16:42:47 +0200

    Add view and basic passport setup

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 import 'dotenv/config';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+// import { Pool } from 'pg'; might be unnecessary, because of prisma-session-store
 import express from 'express';
-const app = express();
+import session from 'express-session';
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import { PrismaClient } from '@prisma/client';
+
 const PORT = process.env.PORT || 3000;
 
-import { PrismaClient } from '@prisma/client';
+const app = express();
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
 const prisma = new PrismaClient();
 
 async function main() {
@@ -22,7 +38,7 @@ main()
 	});
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.render('index');
 });
 
 app.listen(PORT, () => {

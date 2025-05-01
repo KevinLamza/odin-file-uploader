d16bbb7f624ba7c1bc6775f7fb447485ab9f2a77
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2025-05-01 16:14:52 +0200

    Fix wrong output path for prisma client

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function main() {
+	const allUsers = await prisma.users.findMany();
+	console.log(allUsers);
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
+
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
--- prisma/migrations/20250501140808_init/migration.sql
+++ prisma/migrations/20250501140808_init/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
-    "username" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
